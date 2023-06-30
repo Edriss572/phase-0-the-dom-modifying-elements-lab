a340272d1be5289b3820bc242f6c4d5461f8a6f4
Edriss572 <edriss_miakhel@yahoo.com>
2023-06-30 05:56:04 +0300

    The Assigment is done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.setAttribute("YOUR-NAME", "YOUR-NAME");
+newHeader.textContent = "YOUR-NAME" + "is the champion";
+document.body.append(newHeader);
+
+/*
+const element = document.createElement("div");
+const ul = document.createElement('ul');
+
+for(let i = 0;i < 3;i++) {
+  const li = document.createElement('li');
+  li.textContent = (i + 1 ).toString();
+  ul.append(li);
+}
+
+element.append(ul)
+
+document.body.append(element);
+*/
